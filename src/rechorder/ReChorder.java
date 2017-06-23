56bf35cf0caf1fdc69a7c1f80307360a8348a1b2
Dominic <domste10@gmail.com>
2017-06-23 21:50:46 +0100

    Refined the probability process

--- src/rechorder/ReChorder.java
+++ src/rechorder/ReChorder.java
@@ -25,7 +25,8 @@ public class ReChorder {
 	private static int phraseLength = 4;
 	private static double[][] uniProbs = new double[Chords.CHORD_NUMBER][Chords.CHORD_NUMBER];
 	private static double[][][] biProbs = new double[Chords.CHORD_NUMBER][Chords.CHORD_NUMBER][Chords.CHORD_NUMBER];
-	private static double[][] posProbs = new double[phraseLength][Chords.CHORD_NUMBER];
+	private static double[][] posProbs = new double[phraseLength*3][Chords.CHORD_NUMBER];
+	private static double[][] keyProbs = new double[Chords.CHORD_NUMBER][Chords.CHORD_NUMBER];
 	
 	public static void main(String[] args) {
 		for(int i = 0; i < Chords.CHORD_NUMBER; i++){
@@ -54,6 +55,7 @@ public class ReChorder {
 		
 		//First Chord
 		double p = r.nextDouble();
+		System.out.println("p: " + p);
 		for(int i = 0; i < Chords.CHORD_NUMBER; i++){
 			if(posProbs[0][i] >= p){
 				System.out.println("Chord: " + i + ", Probability: " + posProbs[0][i]);
@@ -67,6 +69,7 @@ public class ReChorder {
 		
 		//Second Chord
 		p = r.nextDouble();
+		System.out.println("p: " + p);
 		double q  = r.nextDouble();
 		for(int i = 0; i < Chords.CHORD_NUMBER; i++){
 			if(posProbs[1][i] + uniProbs[sequence.get(0)][i] >= p + q){
@@ -80,22 +83,44 @@ public class ReChorder {
 			}
 		}
 		
+		double[][][][] finalP = calculateP(sequence.get(0));
+		
 		//Other Chords
 		for(int n = 2; n < length; n++){
 			p = r.nextDouble();
-			q  = r.nextDouble();
+			System.out.println("p: " + p);
 			double s = r.nextDouble();
 			for(int i = 0; i < Chords.CHORD_NUMBER; i++){
 				System.out.println("n: " + n + ", i: " + i);
-				if(posProbs[n%phraseLength][i] + uniProbs[sequence.get(n-1)][i] + biProbs[sequence.get(n-2)][sequence.get(n-1)][i] >= p + q + s){
-					System.out.println("Chord: " + i + ", Probability: " + (posProbs[n%phraseLength][i] + uniProbs[sequence.get(n-1)][i] + biProbs[sequence.get(n-2)][sequence.get(n-1)][i])/3);
-					sequence.add(i);
-					break;
+				if(n < phraseLength){
+					if(finalP[n][sequence.get(n-2)][sequence.get(n-1)][i] >= p){
+						System.out.println("Chord: " + i + ", Probability: " + (finalP[n][sequence.get(n-2)][sequence.get(n-1)][i]));
+						sequence.add(i);
+						break;
+					}
+					else{
+						p -= finalP[n][sequence.get(n-2)][sequence.get(n-1)][i];
+					}
+				}
+				else if(length - n < phraseLength){
+					if(finalP[phraseLength*2 + n%4][sequence.get(n-2)][sequence.get(n-1)][i] >= p){
+						System.out.println("Chord: " + i + ", Probability: " + (finalP[phraseLength*2 + n%4][sequence.get(n-2)][sequence.get(n-1)][i]));
+						sequence.add(i);
+						break;
+					}
+					else{
+						p -= finalP[phraseLength*2 + n%4][sequence.get(n-2)][sequence.get(n-1)][i];
+					}
 				}
 				else{
-					p -= posProbs[n%phraseLength][i];
-					q -= uniProbs[sequence.get(n-1)][i];
-					s -= biProbs[sequence.get(n-2)][sequence.get(n-1)][i];
+					if(finalP[phraseLength + n%4][sequence.get(n-2)][sequence.get(n-1)][i] >= p){
+						System.out.println("Chord: " + i + ", Probability: " + (finalP[phraseLength + n%4][sequence.get(n-2)][sequence.get(n-1)][i]));
+						sequence.add(i);
+						break;
+					}
+					else{
+						p -= finalP[phraseLength + n%4][sequence.get(n-2)][sequence.get(n-1)][i];
+					}
 				}
 			}
 		}
@@ -105,7 +130,7 @@ public class ReChorder {
 	private static void refresh(){
 		try {
 			HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory();
-			HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(URL+"Katy Perry"));
+			HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(URL+"The Beatles"));
 			request.setParser(new JsonObjectParser(JSON_FACTORY));
 			HttpHeaders headers = new HttpHeaders();
 		    headers.set("Guitarparty-Api-Key", KEY);
@@ -139,65 +164,103 @@ public class ReChorder {
 	private static void addToTables(Song song) {
 		ArrayList<Integer> chords = (ArrayList<Integer>) song.getChords();
 		for(int i = 0; i < chords.size(); i++){
-			posProbs[i%phraseLength][chords.get(i)] += 1;
+			if(i < phraseLength){
+				posProbs[i][chords.get(i)] += 1;
+			}
+			else if (chords.size() - i < phraseLength){
+				posProbs[(phraseLength * 2) + (i%phraseLength)][chords.get(i)] += 1;
+			}
+			else{
+				posProbs[phraseLength + i%phraseLength][chords.get(i)] += 1;
+			}
 			if(i+1 < chords.size() && !chords.get(i+1).equals(Chords.INVALID)){
 				uniProbs[chords.get(i)][chords.get(i+1)] += 1;
 				if(i+2 < chords.size() && !chords.get(i+2).equals(Chords.INVALID)){
 					biProbs[chords.get(i)][chords.get(i+1)][chords.get(i+2)] += 1;
 				}
 			}
+			keyProbs[chords.get(0)][chords.get(i)] += 1;
 		}
 	}
 	
 	private static void sortProbs() {
-		for(int i = 0; i < phraseLength; i++){
+		for(int i = 0; i < phraseLength*3; i++){
 			double total = 0;
 			for(int j = 0; j < posProbs[i].length; j++){
-				if(posProbs[i][j] == 0){
-					posProbs[i][j] = 0.1;
-					total += 0.1;
-				}
-				else{
-					total += posProbs[i][j];
-				}
+//				if(posProbs[i][j] == 0){
+//					posProbs[i][j] = 0.01;
+//				}
+				total += posProbs[i][j];
 			}
 			for(int j = 0; j < posProbs[i].length; j++){
-				posProbs[i][j] /= total;
+				if(total != 0){
+					posProbs[i][j] /= total;
+				}
 			}
 		}
 		for(int i = 0; i < uniProbs.length; i++){
 			double total = 0;
 			for(int j = 0; j < uniProbs[i].length; j++){
-				if(uniProbs[i][j] == 0){
-					uniProbs[i][j] = 0.1;
-					total += 0.1;
-				}
-				else{
-					total += uniProbs[i][j];
-				}
+//				if(uniProbs[i][j] == 0){
+//					uniProbs[i][j] = 0.01;
+//				}
+				total += uniProbs[i][j];
 			}
 			for(int j = 0; j < uniProbs[i].length; j++){
-				uniProbs[i][j] /= total;
+				if(total != 0){
+					uniProbs[i][j] /= total;
+				}
 			}
 		}
 		for(int i = 0; i < biProbs.length; i++){
-			double total = 0;
 			for(int j = 0; j < biProbs[i].length; j++){
+				double total = 0;
 				for(int k = 0; k < biProbs[i][j].length; k++){
-					if(biProbs[i][j][k] == 0){
-						biProbs[i][j][k] = 0.1;
-						total += 0.1;
-					}
-					else{
-						total += biProbs[i][j][k];
-					}
+//					if(biProbs[i][j][k] == 0){
+//						biProbs[i][j][k] = 0.01;
+//					}
+					total += biProbs[i][j][k];
 				}
 				for(int k = 0; k < biProbs[i][j].length; k++){
-					biProbs[i][j][k] /= total;
+					if(total != total){
+						biProbs[i][j][k] /= total;
+					}
 				}
 			}
-			
 		}
+		for(int i = 0; i < keyProbs.length; i++){
+			double total = 0;
+			for(int j = 0; j < keyProbs[i].length; j++){
+//				if(uniProbs[i][j] == 0){
+//					uniProbs[i][j] = 0.01;
+//				}
+				total += keyProbs[i][j];
+			}
+			for(int j = 0; j < keyProbs[i].length; j++){
+				if(total != 0){
+					keyProbs[i][j] /= total;
+				}
+			}
+		}
+	}
+	
+	private static double[][][][] calculateP(int keyChord){
+	double[][][][] finalP = new double[phraseLength*3][Chords.CHORD_NUMBER][Chords.CHORD_NUMBER][Chords.CHORD_NUMBER];
+		for(int a = 0; a < phraseLength*3; a++){
+			for(int b = 0; b < Chords.CHORD_NUMBER; b++){
+				for(int c = 0; c < Chords.CHORD_NUMBER; c++){
+					double total = 0;
+					for(int d = 0; d < Chords.CHORD_NUMBER; d++){
+						finalP[a][b][c][d] = posProbs[a][d] * uniProbs[c][d] * biProbs[b][c][d] * keyProbs[keyChord][d];
+						total += finalP[a][b][c][d];
+					}
+					for(int d = 0; d < Chords.CHORD_NUMBER; d++){
+						finalP[a][b][c][d] /= total;
+					}
+				}
+			}
+		}
+		return finalP;
 	}
 }
 
